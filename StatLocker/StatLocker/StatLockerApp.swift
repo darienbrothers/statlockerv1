97d34fff6f109141c4b44bd7fa132dedb47374cb
darienbdev <darienbdev@gmail.com>
2025-09-16 16:42:07 -0400

    chore: add dark-first color assets (BrandPrimary, Surface, statuses, accents) + proof view

---
+++ StatLocker/StatLocker/StatLockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatLockerApp.swift
+//  StatLocker
+//
+//  Created by Darien Brothers on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct StatLockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
